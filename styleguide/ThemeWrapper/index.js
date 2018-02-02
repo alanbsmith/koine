bcdee1df2c4fee1c5c315785cb44c01a06d3cb8f
Alan B Smith <alanbsmith@users.noreply.github.com>
2018-02-02 15:13:38 -0700

    [K-55] adds prettier and configureFonts util for docs

--- styleguide/ThemeWrapper/index.js
+++ styleguide/ThemeWrapper/index.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
-import { ThemeProvider } from 'styled-components';
-import { theme }  from 'koine-ui-utils';
-import FontFaceObserver from 'fontfaceobserver';
+import React, { Component } from "react";
+import { ThemeProvider, injectGlobal } from "styled-components";
+import { theme, buildGlobalStyles } from "koine-ui-utils";
+
+import configureFonts from "config/configureFonts";
+
+injectGlobal([buildGlobalStyles(theme)]);
+configureFonts(theme);
 
 export default class Wrapper extends Component {
   render() {
-    return <ThemeProvider theme={theme}>{this.props.children}</ThemeProvider>
+    return <ThemeProvider theme={theme}>{this.props.children}</ThemeProvider>;
   }
 }
